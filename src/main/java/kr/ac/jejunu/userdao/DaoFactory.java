074b3117371f483fe060c2f4ff3b0563aced834e
WonBeomChoi <cwb2310@gmail.com>
2019-04-19 17:04:09 +0900

    6수정

--- src/main/java/kr/ac/jejunu/userdao/DaoFactory.java
+++ src/main/java/kr/ac/jejunu/userdao/DaoFactory.java
@@ -1,22 +1,40 @@
 package kr.ac.jejunu.userdao;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 import javax.sql.DataSource;
+import java.sql.Driver;
 import java.sql.SQLException;
 
 @Configuration
 public class DaoFactory {
 
+    @Value("${db.classname}")
+    String className = "com.mysql.cj.jdbc.Driver";
+    @Value("${db.url}")
+    String url = "jdbc:mysql://localhost/jeju?serverTimezone=UTC";
+    @Value("${db.username}")
+    String userName = "root";
+    @Value("${db.password}")
+    String password = "dnjs2310!@";
+
     @Bean
     public UserDao getUserDao() throws SQLException, ClassNotFoundException {
         return new UserDao(getDataSource());
     }
-
     @Bean
-    public DataSource getDataSource() throws SQLException, ClassNotFoundException {
-        ConnectionMaker connectionMaker = new JejuConnectionMaker();
-        return connectionMaker.getConnection();
+    public DataSource getDataSource() throws ClassNotFoundException {
+
+
+        SimpleDriverDataSource simpleDriverDataSource = new SimpleDriverDataSource();
+        simpleDriverDataSource.setDriverClass((Class<? extends Driver>) Class.forName(className));
+        simpleDriverDataSource.setUsername(userName);
+        simpleDriverDataSource.setPassword(password);
+        simpleDriverDataSource.setUrl(url);
+
+        return simpleDriverDataSource;
     }
     }
