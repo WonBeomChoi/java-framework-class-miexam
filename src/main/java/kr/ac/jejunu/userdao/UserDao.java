6657b11915fc97b6ce4a62dd6d6c9047987e1000
WonBeomChoi <cwb2310@gmail.com>
2019-04-19 21:31:43 +0900

    11

--- src/main/java/kr/ac/jejunu/userdao/UserDao.java
+++ src/main/java/kr/ac/jejunu/userdao/UserDao.java
@@ -9,19 +9,39 @@ public class UserDao {
         context = new Context(dataSource);
     }
     public User get(Long id) throws SQLException {
-        StatementStrategy statementStrategy = new GetStatementStrategy(id);
+        StatementStrategy statementStrategy = connection -> {
+            PreparedStatement preparedStatement = connection.prepareStatement("select * from userinfo where id = ?");
+            preparedStatement.setLong(1, id);
+            return preparedStatement;
+        };
         return context.getContext(statementStrategy);
     }
     public Long add(User user) throws SQLException {
-        StatementStrategy statementStrategy = new AddStatmentStrategy(user);
+        StatementStrategy statementStrategy = connection -> {
+            PreparedStatement preparedStatement = connection.prepareStatement("insert into userinfo(name,password) values (?,?)");
+            preparedStatement.setString(1,user.getName());
+            preparedStatement.setString(2, user.getPassword());
+            return preparedStatement;
+        };
         return context.addContext(statementStrategy);
     }
     public void update(User user) throws SQLException {
-        StatementStrategy statementStrategy = new UpdateStatementStrategy(user);
+        StatementStrategy statementStrategy = connection -> {
+            PreparedStatement preparedStatement = connection.prepareStatement("update userinfo set name =?, password=? where id =?");
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getPassword());
+            preparedStatement.setLong(3, user.getId());
+
+            return preparedStatement;
+        };
         context.updateContext(statementStrategy);
     }
     public void delete(Long id) throws SQLException{
-        StatementStrategy statementStrategy = new DeleteStatementStrategy(id);
+        StatementStrategy statementStrategy = connection -> {
+            PreparedStatement preparedStatement = connection.prepareStatement("delete from userinfo where id = ?");
+            preparedStatement.setLong(1, id);
+            return preparedStatement;
+        };
         context.updateContext(statementStrategy);
     }
 }
